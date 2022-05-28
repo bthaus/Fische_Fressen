458836a72a1034f026f2b6787bcec9511a794b97
alexlaggner <allaggner@edu.aau.at>
2022-05-28 11:29:41 +0200

    Alex: Code Cleanup: Fischadapter, Movement, Fishcontainer, GameScreen

--- app/src/main/java/com/example/fische_fressen/GameModels/Movement.java
+++ app/src/main/java/com/example/fische_fressen/GameModels/Movement.java
@@ -3,10 +3,20 @@ package com.example.fische_fressen.GameModels;
 public class Movement {
     Position position;
    public  enum Direction{
-        RIGHT,
-        LEFT,
-        UP,
-        DOWN
-    }
+        RIGHT(1),
+        LEFT(-1),
+        UP(-5),
+        DOWN(5);
+
+       private Direction(int directionValue){
+            this.directionOffset = directionValue;
+       }
+
+       private int directionOffset;
+
+       public int getDirectionOffset() {
+           return directionOffset;
+       }
+   }
 
 }
