76a2fc3f392645f8a0685da5e803293e0b781ec5
bthaus <bwuest@gmx.at>
2022-06-08 13:01:56 +0200

    bodo: added winscreen

--- app/src/main/java/com/example/fische_fressen/WinScreen.java
+++ app/src/main/java/com/example/fische_fressen/WinScreen.java
@@ -1,7 +1,9 @@
 package com.example.fische_fressen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.fische_fressen.utils.Global;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,11 +24,18 @@ public class WinScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Global.playing=false;
         super.onCreate(savedInstanceState);
 
         binding = ActivityWinScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        if(Global.won){
+            binding.winorlose.setText(R.string.game_won);
+        }else{
+            binding.winorlose.setText(R.string.game_lost);
+        }
+        binding.totalpoints.setText(String.valueOf(Global.scorePoints));
+        binding.backtomainmenue.setOnClickListener(view -> startActivity(new Intent(view.getContext(), MainActivity.class)));
 
 
 
--- app/src/main/java/com/example/fische_fressen/utils/Global.java
+++ app/src/main/java/com/example/fische_fressen/utils/Global.java
@@ -21,8 +21,11 @@ public class Global {
     public static int lastClickedPosition=-1;
     public static Fish defaultFish=new Fish(R.drawable.empty,-2);
     public static int scorePoints=0;
-    public static int difficultyValue=150;
+    public static int difficultyValue=90;
     public static Difficulty difficulty=Difficulty.HARD;
+    public static boolean won=false;
+    public static boolean playing;
+
 
     public static boolean isSleepytime() {
         return sleepytime;
@@ -68,7 +71,7 @@ public class Global {
 
     public static Fish getRandomFish(){
         int rand = (int) (Math.random() * 1000);
-        if(rand>950){
+        if(rand>0){
             return new ExplosionFish();
         }
         rand=rand%3;
